9eb6aa8185e0d63c2a8d94444cc1133b7b158e23
Hernandes <Hernandeshass910@gmail.com>
2024-09-19 23:50:55 -0300

    feat: adicionar verificação primeiro digito cpf

---
+++ verificadorCpf.py
@@ -0,0 +1,24 @@
+# cpf 469.743.048-46
+
+cpf = "469.743.048-46"
+cpfNumero = ""
+soma = 0
+i = 10
+for caractere in cpf:
+    try:
+        int(caractere)
+        cpfNumero += caractere
+    except:
+        ...
+
+cpfNumero = cpfNumero[:9]
+
+for numero in cpfNumero:
+    soma += int(numero) * i
+    i -= 1
+
+multi = soma*10
+
+resto = multi % 11
+
+print(resto)
