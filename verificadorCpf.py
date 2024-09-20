7ce3c0879c61f159c794a5d26015441c7beb430c
Hernandes <Hernandeshass910@gmail.com>
2024-09-20 01:06:25 -0300

    feat: adicionando verificação segundo dígito
    Adicionando também:
     - Comentários
     - Mudando nome de variáveis
     - Operação ternária segundo digito
     - cpf iniciando como variável str

--- verificadorCpf.py
+++ verificadorCpf.py
@@ -1,22 +1,43 @@
 # cpf 469.743.048-46
 
-cpf = input("Digite o seu cpf, inclua pontos e traço: ")
+cpf = "469.743.048-46"
+
+#declarando variáveis
 
 cpf = cpf[:-2]
 cpfNumero = ""
-soma = 0
-i = 10
+soma1 = 0
+soma2 = 0
+contador1 = 10
+contador2 = 11
+
+#descobrindo cpf sem caracteres especiais e fazendo a primeira soma
 
 for numero in cpf:
     try:
+        int(numero)
+        cpfNumero += numero
         numero = int(numero)
-        soma += numero * i
-        i -= 1
+        soma1 += numero * contador1
+        contador1 -= 1
     except:
         ...
 
-digito = soma * 10 % 11
+#descobrindo o primeiro digito
 
-digito = digito if digito <=9 else 0
+digito1 = soma1 * 10 % 11
 
-print(digito)
+digito1 = digito1 if digito1 <=9 else 0
+
+#descobrindo o segundo digito
+
+cpfNumero = int(cpfNumero) + digito1
+
+for numero in str(cpfNumero):
+    soma2 += int(numero) * contador2
+    contador2 -= 1
+
+digito2 = soma2 * 10 % 11
+digito2 = digito2 if digito2 <=9 else 0
+
+print(digito2)
